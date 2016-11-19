6969f4357db7cd35a4e3dd482d4846b52f78df7b
yanap <yanap1214@gmail.com>
2016-11-19 20:35:35 +0900

    modify view;wq;

---
+++ app/src/main/java/com/yanap/besttiming/CardRecyclerView.java
@@ -0,0 +1,22 @@
+package com.yanap.besttiming;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by yanap on 2016/11/19.
+ */
+
+public class CardRecyclerView extends RecyclerView {
+    public CardRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setRecyclerAdapter(context);
+    }
+
+    public void setRecyclerAdapter(Context context){
+        setLayoutManager(new LinearLayoutManager(context));
+        setAdapter(new CardRecyclerAdapter(context,context.getResources().getStringArray(R.array.dummy)));
+    }
+}
